f81a73d745c10d7aec197e5d4ca42d3be94f38c2
kp-kepra <kevinpr153@gmail.com>
2018-12-04 00:14:29 -0500

    updated minimal readme

---
+++ README.md
@@ -0,0 +1,14 @@
+# ICLR 2019 Reproducibility Challenge
+
+## Overview
+Original Paper : https://openreview.net/pdf?id=SJeFNoRcFQ
+Repository for Reproducibility Challenge.
+
+Tools:
+- PyTorch
+- Powerlaw
+- Numpy
+- Scipy
+- Scikit-learn
+
+Rest of Readme WIP
--- main.py
+++ main.py
@@ -4,17 +4,17 @@ from tools import *
 import trainer
 
 # MiniAlexNet epoch : 10
-# MLP3 epoch : 50
-# AllenNLP
+MLP3 epoch : 50
+AllenNLP
 
 # LeNet Training
-# batch_list = [128]
-# trainer.train('lenet', 'mnist', batch_list, 20, 'adadelta')
+batch_list = [128]
+trainer.train('lenet', 'mnist', batch_list, 20, 'adadelta')
 
 # MiniALexNet Training
-# batch_list = [500, 250, 100, 32, 16, 8, 4, 2]
-# trainer.train('minialexnet', 'cifar10', batch_list, 10, 'sgd', False)
+batch_list = [500, 250, 100, 32, 16, 8, 4, 2]
+trainer.train('minialexnet', 'cifar10', batch_list, 10, 'sgd', False)
 
 # MLP3 Training
-# batch_list = [16]
-# trainer.train('mlp3', 'cifar10', batch_list, 50, 'sgd', True)
+batch_list = [16]
+trainer.train('mlp3', 'cifar10', batch_list, 50, 'sgd', True)
